27bf039eeb869e41b82fb4dafdef2e92e568039a
Gustavo Santos <gustavojuneo@outlook.com>
2021-03-22 11:41:49 -0300

    Create OAuth with Github and NextAuth

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,14 @@
-import { GetServerSideProps } from 'next'
+import { GetStaticProps } from 'next'
 import Head from 'next/head'
 import { stripe } from '../services/stripe'
 
 import { SubscribeButton } from '../components/SubscribeButton'
 
 import styles from './home.module.scss'
+
+// Client-side
+// Server-side
+// Static Site Generation
 
 interface HomeProps {
   product: {
@@ -39,7 +43,7 @@ export default function Home({ product }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1IXnoqEIS1lkQIzSZVX0kQF9')
 
   const product = {
@@ -53,6 +57,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       product
-    }
+    },
+    revalidate: 60 * 60 * 24 // 24 hours
   }
 }
