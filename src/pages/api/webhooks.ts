68358a060d30f4e4bb4e670480b0029a12c99335
Gustavo Santos <gustavojuneo@outlook.com>
2021-03-22 19:50:57 -0300

    Finished Stripe webhooks

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -1,3 +1,4 @@
+import { Subscription } from 'faunadb/src/types/Stream'
 import { NextApiRequest, NextApiResponse } from 'next'
 import { Readable } from 'stream'
 import Stripe from 'stripe'
@@ -20,7 +21,11 @@ export const config = {
   }
 }
 
-const relevantEvents = new Set(['checkout.session.completed'])
+const relevantEvents = new Set([
+  'checkout.session.completed',
+  'customer.subscription.updated',
+  'customer.subscription.deleted'
+])
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === 'POST') {
@@ -44,12 +49,23 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     if (relevantEvents.has(type)) {
       try {
         switch (type) {
+          case 'customer.subscription.updated':
+          case 'customer.subscription.deleted':
+            const subscription = event.data.object as Stripe.Subscription
+
+            await saveSubscription(
+              subscription.id,
+              subscription.customer.toString()
+            )
+
+            break
           case 'checkout.session.completed':
             const checkoutSession = event.data.object as Stripe.Checkout.Session
 
             await saveSubscription(
               checkoutSession.subscription.toString(),
-              checkoutSession.customer.toString()
+              checkoutSession.customer.toString(),
+              true
             )
 
             break
