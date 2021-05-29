bd0ee04396de2f5ff1a2b48abe60957376e5b57d
Gustavo Juneo Santos <gustavojuneo@outlook.com>
2021-05-29 12:24:28 -0300

    Create tests for components

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,38 +1,34 @@
-import { useSession, signIn } from 'next-auth/client'
-import { useRouter } from 'next/router'
-import { api } from '../../services/api'
-import { getStripeJs } from '../../services/stripe-js'
-import styles from './styles.module.scss'
+import { useSession, signIn } from 'next-auth/client';
+import { useRouter } from 'next/router';
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
+import styles from './styles.module.scss';
 
-interface SubscribeButtonProps {
-  priceId: string
-}
-
-export function SubscribeButton({ priceId }: SubscribeButtonProps) {
-  const [session] = useSession()
-  const router = useRouter()
+export function SubscribeButton() {
+  const [session] = useSession();
+  const router = useRouter();
 
   async function handleSubscribe() {
     if (!session) {
-      signIn('github')
-      return
+      signIn('github');
+      return;
     }
 
     if (session?.activeSubscription) {
-      router.push('/posts')
-      return
+      router.push('/posts');
+      return;
     }
 
     try {
-      const response = await api.post('/subscribe')
+      const response = await api.post('/subscribe');
 
-      const { sessionId } = response.data
+      const { sessionId } = response.data;
 
-      const stripe = await getStripeJs()
+      const stripe = await getStripeJs();
 
-      await stripe.redirectToCheckout({ sessionId })
+      await stripe.redirectToCheckout({ sessionId });
     } catch (err) {
-      alert(err.message)
+      alert(err.message);
     }
   }
 
@@ -44,5 +40,5 @@ export function SubscribeButton({ priceId }: SubscribeButtonProps) {
     >
       Subscribe Now
     </button>
-  )
+  );
 }
