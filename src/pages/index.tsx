257d2e23adb21de99d7177cccb3b87eedabee1da
Gustavo Juneo Santos <gustavojuneo@outlook.com>
2021-05-31 14:12:17 -0300

    Create test for home and posts page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,10 @@
-import { GetStaticProps } from 'next'
-import Head from 'next/head'
-import { stripe } from '../services/stripe'
+import { GetStaticProps } from 'next';
+import Head from 'next/head';
+import { stripe } from '../services/stripe';
 
-import { SubscribeButton } from '../components/SubscribeButton'
+import { SubscribeButton } from '../components/SubscribeButton';
 
-import styles from './home.module.scss'
+import styles from './home.module.scss';
 
 // Client-side
 // Server-side
@@ -12,9 +12,9 @@ import styles from './home.module.scss'
 
 interface HomeProps {
   product: {
-    priceId: string
-    amount: number
-  }
+    priceId: string;
+    amount: string;
+  };
 }
 
 export default function Home({ product }: HomeProps) {
@@ -34,30 +34,30 @@ export default function Home({ product }: HomeProps) {
             Get access to all the publications <br />
             <span>for {product.amount} month</span>
           </p>
-          <SubscribeButton priceId={product.priceId} />
+          <SubscribeButton />
         </section>
 
         <img src="/images/avatar.svg" alt="Girl coding" />
       </main>
     </>
-  )
+  );
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-  const price = await stripe.prices.retrieve('price_1IXnoqEIS1lkQIzSZVX0kQF9')
+  const price = await stripe.prices.retrieve('price_1IXnoqEIS1lkQIzSZVX0kQF9');
 
   const product = {
     priceId: price.id,
     amount: new Intl.NumberFormat('en-US', {
       style: 'currency',
-      currency: 'USD'
-    }).format(price.unit_amount / 100)
-  }
+      currency: 'USD',
+    }).format(price.unit_amount / 100),
+  };
 
   return {
     props: {
-      product
+      product,
     },
-    revalidate: 60 * 60 * 24 // 24 hours
-  }
-}
+    revalidate: 60 * 60 * 24, // 24 hours
+  };
+};
